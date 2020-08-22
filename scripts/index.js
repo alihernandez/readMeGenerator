64585fe49a9d7dcb195aa0b40fa3ed6988a6716c
phantomramen <ali.hernandez9two@gmail.com>
2020-08-22 13:39:59 -0600

    prefinal

--- scripts/index.js
+++ scripts/index.js
@@ -6,115 +6,117 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 function promptUser() {
   return inquirer.prompt([
-// * Title
-// * Description
-// * Table of Contents
-// * Installation
-// * Usage
-// * License
-// * Contributing
-// * Tests
-// * Questions
-{
-  type: "input",
-  name: "title",
-  message: "hello what's the title of the project?",
-},
-{
-  type: "input",
-  name: "description",
-  message: "please enter a short description of your project",
-},
-// {
-//   type: "input",
-//   name: "table-of-contents",
-//   message: "please enter items for table of contents",
-// },
+    {
+      type: "input",
+      name: "title",
+      message: "hello what's the title of the project?",
+    },
 
-{
-  type: "input",
-  name: "installation",
-  message: "please enter the installation",
-},
+    {
+      type: "input",
+      name: "questions",
+      message: "hello whats your github username?",
+    },
 
-{
-  type: "input",
-  name: "usage",
-  message: "usage?",
-},
+    {
+      type: "input",
+      name: "description",
+      message: "please enter a short description of your project",
+    },
 
-{
-  type: "list",
-  name: "license",
-  message: "any licenses?",
-  choices: ["MIT", "Mozilla", "IBM"],
-},
-{
-  type: "input",
-  name: "contributing",
-  message: "contributing",
-},
-{
-  type: "input",
-  name: "questions",
-  message: "hello whats your name?",
-},
+    {
+      type: "input",
+      name: "installation",
+      message: "please enter command required to install the project:",
+    },
+
+    {
+      type: "input",
+      name: "usage",
+      message: "please provied instructions and examples for use:",
+    },
+
+    {
+      type: "list",
+      name: "license",
+      message: "please select applicable licenses:",
+      choices: ["MIT", "Mozilla", "IBM"],
+    },
+    {
+      type: "input",
+      name: "credits",
+      message: "List your collaborators, if any.",
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message:
+        "If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so.",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message:
+        "If you know how to run tests include them here:",
+    },
   ]);
 }
 
 function generateMarkdown(answers) {
   return `
-# ${answers.title}
+# **${answers.title}**
 
-## Badges
+${
+  answers.license === "MIT"
+    ? "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    : answers.license === "IBM"
+    ? "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"
+    : "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+} 
 
-![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-
-Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
-## License
-   ${answers.license === "MIT" ? "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)" :  answers.license === "IBM" ? "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)":  "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"} 
-
-
+## gitHub portfolio:
+https://github.com/${answers.questions}
 
 ## Description 
+------------
 ${answers.description}
 
-
-## Table of Contents (Optional)
-
-If your README is very long, add a table of contents to make it easy for users to find what they need.
-
+## Table of Contents 
+------------
 * [Installation](#installation)
 * [Usage](#usage)
 * [Credits](#credits)
 * [License](#license)
 
-
 ## Installation
+------------
+${answers.installation} 
 
-${answers.installation}
-
-   
 ## Usage 
-
-Provide instructions and examples for use. Include screenshots as needed.  
-
+------------
+${answers.usage}
 
 ## Credits
+------------
+${answers.credits}
 
+## License
+------------
+   ${
+     answers.license === "MIT"
+       ? "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+       : answers.license === "IBM"
+       ? "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"
+       : "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+   } 
+  
+## Contributing
+------------
 ${answers.contributing}
 
-
-## Contributing
-
-If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
-
 ## Tests
-
-Go the extra mile and write tests for your application. Then provide examples on how to run them.
-
-
+------------
+${answers.tests}
 `;
 }
 
