ae8c3699ea17cd961ab875fe6a6d2a8fa3cddeb2
phantomramen <ali.hernandez9two@gmail.com>
2020-08-22 10:06:34 -0600

    skeleton

--- scripts/index.js
+++ scripts/index.js
@@ -6,83 +6,119 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 function promptUser() {
   return inquirer.prompt([
-    // * Title
-    // * Description
-    // * Table of Contents
-    // * Installation
-    // * Usage
-    // * License
-    // * Contributing
-    // * Tests
-    // * Questions
-    {
-      type: "input",
-      name: "title",
-      message: "hello what's the title of the project?",
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "please enter a short description of your project",
-    },
-    {
-      type: "input",
-      name: "table-of-contents",
-      message: "please enter items for table of contents",
-    },
+// * Title
+// * Description
+// * Table of Contents
+// * Installation
+// * Usage
+// * License
+// * Contributing
+// * Tests
+// * Questions
+{
+  type: "input",
+  name: "title",
+  message: "hello what's the title of the project?",
+},
+{
+  type: "input",
+  name: "description",
+  message: "please enter a short description of your project",
+},
+// {
+//   type: "input",
+//   name: "table-of-contents",
+//   message: "please enter items for table of contents",
+// },
 
-    {
-      type: "input",
-      name: "installation",
-      message: "please enter the installation",
-    },
+{
+  type: "input",
+  name: "installation",
+  message: "please enter the installation",
+},
 
-    {
-      type: "input",
-      name: "usage",
-      message: "usage?",
-    },
+{
+  type: "input",
+  name: "usage",
+  message: "usage?",
+},
 
-    {
-      type: "input",
-      name: "license",
-      message: "any licenses?",
-    },
-    {
-        type: "input",
-        name: "contributing",
-        message: "contributing"
-        },
-        {
-            type: "input",
-            name: "questions",
-            message: "hello whats your name?"
-            },
-
+{
+  type: "list",
+  name: "license",
+  message: "any licenses?",
+  choices: ["MIT", "Mozilla", "IBM"],
+},
+{
+  type: "input",
+  name: "contributing",
+  message: "contributing",
+},
+{
+  type: "input",
+  name: "questions",
+  message: "hello whats your name?",
+},
   ]);
-};
-
-function generateMarkdown(answers){
-    return `
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<h1>#${answers.title}</h1>
-<p>*${answers.description}*<p>
-
-    
-</body>
-</html>
-    `
 }
 
-promptUser()
-.then(function(answers){
-    const html = generateMarkdown(answers);
-    return writeFileAsync("markUp.html", html);
+function generateMarkdown(answers) {
+  return `
+# ${answers.title}
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+
+Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## License
+   ${answers.license === "MIT" ? "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)" :  answers.license === "IBM" ? "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)":  "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"} 
+
+
+
+## Description 
+${answers.description}
+
+
+## Table of Contents (Optional)
+
+If your README is very long, add a table of contents to make it easy for users to find what they need.
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+
+## Installation
+
+${answers.installation}
+
+   
+## Usage 
+
+Provide instructions and examples for use. Include screenshots as needed.  
+
+
+## Credits
+
+${answers.contributing}
+
+
+## Contributing
+
+If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them.
+
+
+`;
+}
+
+promptUser().then(function (answers) {
+  const markdown = generateMarkdown(answers);
+  return writeFileAsync("readMe.md", markdown);
 });
